b93bcfbe37d0446be76ae27e118bf947f26b8d1a
Administrator <3183848638@qq.com>
2021-11-04 09:46:21 +0800

    learn

--- spring-servlet/src/main/java/controller/HelloController.java
+++ spring-servlet/src/main/java/controller/HelloController.java
@@ -7,12 +7,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
     public HelloController() {
-        System.out.println("被调用了");
     }
 
     @RequestMapping("/hello")
     public String hello(Model model) {
-        System.out.println("被调用了");
         //封装数据
         model.addAttribute("msg", "Hello SpringMVCAnnotation");
         return "hello";
